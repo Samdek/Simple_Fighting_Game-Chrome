880bc537de443e32913919fc540582265d808312
Adekoya Samuel <99253410+Samdek@users.noreply.github.com>
2022-07-17 23:50:36 +0100

    final commit

---
+++ index.js
@@ -0,0 +1,174 @@
+const characterData = {
+    hero: {
+        name: "Wizard",
+        avatar: "img/Wizard.jpeg",
+        health: 80,
+        diceCount: 3,
+        currentDiceScore: []
+    },
+    orc: {
+        name: "Orc",
+        avatar: "img/Orc.jpeg",
+        health: 40,
+        diceCount: 1,
+        currentDiceScore: []
+    },
+    demon: {
+        name: "Demon",
+        avatar: "img/Demon.jpeg",
+        health: 35,
+        diceCount: 2,
+        currentDiceScore: []
+    },
+    goblin: {
+        name: "Goblin",
+        avatar: "img/Goblin.jpeg",
+        health: 30,
+        diceCount: 3,
+        currentDiceScore: []
+    }
+}
+
+
+///////*****utilities********/////////
+function getDiceRollArray(diceCount) {
+    return new Array(diceCount).fill(0).map(() =>
+        Math.floor(Math.random() * 6) + 1
+    )
+}
+
+const getPercentage = (remainingHealth, maximumHealth) => 
+    (100 * remainingHealth) / maximumHealth
+
+function getDicePlaceholderHtml(diceCount) {
+    return new Array(diceCount).fill(0).map(() =>
+        `<div class="placeholder-dice"></div>`
+    ).join("")
+}
+
+
+//////////******Character***/////////////
+class Character {
+    constructor(data) {
+        Object.assign(this, data)
+        this.maxHealth = this.health
+        this.diceHtml = getDicePlaceholderHtml(this.diceCount)
+    }
+
+    setDiceHtml() {
+        this.currentDiceScore = getDiceRollArray(this.diceCount)
+        this.diceHtml = this.currentDiceScore.map((num) =>
+            `<div class="dice">${num}</div>`).join("")
+    }
+
+    takeDamage(attackScoreArray) {
+        const totalAttackScore = attackScoreArray.reduce((total, num) => total + num)
+        this.health -= totalAttackScore
+        if (this.health <= 0) {
+            this.dead = true
+            this.health = 0
+        }
+    }
+
+    getHealthBarHtml() {
+        const percent = getPercentage(this.health, this.maxHealth)
+        return `<div class="health-bar-outer">
+                    <div class="health-bar-inner ${percent < 26 ? "danger" : ""}" 
+                            style="width:${percent}%;">
+                    </div>
+                </div>`
+    }
+
+    getCharacterHtml() {
+        const { elementId, name, avatar, health, diceCount, diceHtml } = this
+        const healthBar = this.getHealthBarHtml()
+        return `
+            <div class="character-card">
+                <h4 class="name"> ${name} </h4>
+                <img class="avatar" src="${avatar}" />
+                <div class="health">health: <b> ${health} </b></div>
+                ${healthBar}
+                <div class="dice-container">
+                    ${diceHtml}
+                </div>
+            </div>`
+    }
+}
+
+
+//////////******index*****////////
+let monstersArray = ["orc", "demon", "goblin"]
+let isWaiting = false
+
+function getNewMonster() {
+    const nextMonsterData = characterData[monstersArray.shift()]
+    return nextMonsterData ? new Character(nextMonsterData) : {}
+}
+
+function attack() {
+    if(!isWaiting){
+        wizard.setDiceHtml()
+        monster.setDiceHtml()
+        wizard.takeDamage(monster.currentDiceScore)
+        monster.takeDamage(wizard.currentDiceScore)
+        render()
+        
+        if(wizard.dead){
+            endGame()
+        }
+        else if(monster.dead){
+            isWaiting = true
+            if(monstersArray.length > 0){
+                setTimeout(()=>{
+                    monster = getNewMonster()
+                    render()
+                    isWaiting = false
+                },1500)
+            }
+            else{
+                endGame()
+            }
+        }    
+    }
+}
+
+function endGame() {
+    isWaiting = true
+    const endMessage = wizard.health === 0 && monster.health === 0 ?
+        "No victors - all creatures are dead" :
+        wizard.health > 0 ? "The Wizard Wins" :
+            "The monsters are Victorious"
+
+    const endEmoji = wizard.health > 0 ? "🔮" : "☠️"
+        setTimeout(()=>{
+            document.body.innerHTML = `
+                <div class="end-game">
+                    <h2>Game Over</h2> 
+                    <h3>${endMessage}</h3>
+                    <p class="end-emoji">${endEmoji}</p>
+                </div>
+                `
+        }, 1500)
+}
+
+document.getElementById("attack-button").addEventListener('click', attack)
+
+function render() {
+    document.getElementById('hero').innerHTML = wizard.getCharacterHtml()
+    document.getElementById('monster').innerHTML = monster.getCharacterHtml()
+}
+
+const wizard = new Character(characterData.hero)
+let monster = getNewMonster()
+render()
+
+
+
+
+
+
+
+
+
+
+
